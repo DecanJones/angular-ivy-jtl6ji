2838ee002822b5eec3af74011b8094c7c39b2588
DecanJones <marquis.a.palmer@gmail.com>
2021-12-03 11:58:34 -0500

    21 files added

---
+++ src/app/core/global-error-handler/global-error-handler.service.ts
@@ -0,0 +1,35 @@
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+
+import * as StackTrace from 'stacktrace-js';
+import { LogService } from '../log/log.service';
+
+@Injectable()
+export class GlobalErrorHandler extends ErrorHandler {
+  constructor(private injector: Injector) {
+    super();
+  }
+
+  public handleError(error) {
+    const logService: LogService = this.injector.get(LogService);
+    const message = error.message ? error.message : error.toString();
+
+    if (error.status) {
+      error = new Error(message);
+    }
+
+    StackTrace.fromError(error).then((stackframes) => {
+      const stackString = stackframes
+        .splice(0, 10)
+        .map(function(sf) {
+          return sf.toString();
+        })
+        .toString();
+
+      const errorTraceStr = `Error message: ${message}. Stack trace: ${stackString}`;
+
+      logService.logError(errorTraceStr);
+
+      throw error;
+    });
+  }
+}
